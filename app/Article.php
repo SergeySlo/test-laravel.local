3fc1b88bc41ed6b76399e618b8989d765154d0e1
Sergey <myemail@example.com>
2021-06-23 15:27:10 +0600

    soft delete for articles

--- app/Article.php
+++ app/Article.php
@@ -3,12 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'text',
     ];
+
+    protected $dates = ['deleted_at'];
 
     public function users()
     {
